a5d0f3b5146b90f2a1a840f8dc7f23e5e36e9b42
mamat1815 <mbahgendengngegame@gmail.com>
2023-12-04 01:59:47 +0700

    Initial commit

---
+++ app/src/main/java/com/ndp/picodiploma/shoejima/ui/CobaActivity.kt
@@ -0,0 +1,12 @@
+package com.ndp.picodiploma.shoejima.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ndp.picodiploma.shoejima.R
+
+class CobaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_coba)
+    }
+}
